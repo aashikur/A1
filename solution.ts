0e1f2a3946ca3487510beb907882dbad51d6e850
aashikur <mdaashikur@gmail.com>
2025-11-19 01:31:32 +0600

    final

--- solution.ts
+++ solution.ts
@@ -4,6 +4,16 @@ const formateValue = (value: string | number | boolean): string | number | boole
   if (typeof value === 'boolean') return !value;
 }
 
+
+
+
+
+
+
+
+
+
+
 const getLength = (input: string | any[]): number => {
   if (typeof input === 'string') return input.length;
   if (Array.isArray(input)) return input.length;
@@ -11,6 +21,14 @@ const getLength = (input: string | any[]): number => {
   const fallback: never = input;
   return fallback;
 }
+
+
+
+
+
+
+
+
 
 class Person {
   name: string;
@@ -26,6 +44,17 @@ class Person {
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+
 interface Books {
   title: string;
   rating: number;
@@ -34,6 +63,18 @@ interface Books {
 const filterByRating = (items: Books[]): Books[] => {
   return items.filter(item => item.rating >= 4);
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 interface Users {
   id: number;
@@ -45,6 +86,65 @@ interface Users {
 const filterActiveUsers = (users: Users[]): Users[] => {
   return users.filter(users => users.isActive === true);
 }
+
+
+
+
+
+// PrintBook Details
+/*
+Problem 6:
+Define an interface Book with the following properties:
+
+title (string)
+author (string)
+publishedYear (number)
+isAvailable (boolean)
+Then, create a function printBookDetails that accepts an object of type Book and prints its details to the console in the format: "Title: [title], Author: [author], Published: [publishedYear], Available: [Yes/No]".
+
+Requirements:
+You must define the Book interface correctly.
+The printBookDetails function must accept an object that follows to the Book interface.
+Sample Input:
+const myBook: Book = {
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  publishedYear: 1925,
+  isAvailable: true,
+};
+
+printBookDetails(myBook);
+Sample Output:
+Title: The Great Gatsby, Author: F. Scott Fitzgerald, Published: 1925, Available: Yes
+
+*/
+
+
+interface Book {
+  title : string;
+  author: string;
+  publishedYear: number;
+  isAvailable : boolean;
+}
+
+const printBookDetails = (myBook : Book)  => {
+  return `Title: ${myBook.title}, Author: ${myBook.author}, Published: ${myBook.publishedYear}, Available: ${myBook.isAvailable? "Yes": "No"}`
+}
+
+const myBook: Book = {
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  publishedYear: 1925,
+  isAvailable: true,
+};
+
+const x = printBookDetails(myBook);
+console.log(x)
+
+
+ 
+
+
 
 const getUniqueValues = <T>(arr1: T[], arr2: T[]): T[] => {
   let result: T[] = []; 
@@ -70,6 +170,15 @@ const getUniqueValues = <T>(arr1: T[], arr2: T[]): T[] => {
 
   return result;
 };
+
+
+
+
+
+
+
+
+
 
 type Product = {
   name: string;
